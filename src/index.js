ebff04f89986634522dbbefae715c5077b563fe0
JayJay-H <0911aza@naver.com>
2019-12-15 01:18:12 +0900

    #1.2

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo";
+import App from './App';
+import "./globalStyles";
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
